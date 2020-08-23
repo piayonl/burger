42821bbe5cd7f18664ed4544b9bd26349b8f4373
piayonl <piayonl@yahoo.com>
2020-08-23 09:29:46 -0400

    commit

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### Schema
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id INT NOT NULL AUTO_INCREMENT,
+	burger_name varchar(355) NOT NULL,
+	hungry BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Lamb Burger');
+INSERT INTO burgers (burger_name) VALUES ('Waygu Burger');
+INSERT INTO burgers (burger_name) VALUES ('Classic Cheeseburger');
+INSERT INTO burgers (burger_name, hungry) VALUES ('Veal Burger', true);
+INSERT INTO burgers (burger_name, hungry) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, hungry) VALUES ('Fries Burger', true);
